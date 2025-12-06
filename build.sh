48d176018ac11785d24c2277f6ffcae87d9a9a72
sumitgit01 <sumitjoshi1988@gmail.com>
2025-12-06 17:22:01 +0530

    added build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-DOCKER_REGISTRY="trialcpnyac.jfrog.io"
-USERNAME="amitajoshi1992@gmail.com"
-PASSWORD="YW1pdGFqb3NoaTE5OTJAZ21haWwuY29tOmNtVm1kR3R1T2pBeE9qRTNPVFkxTlRZMk5EZzZSV1JvVkhFMFZtaEVhbEJNUmtWSFdrazBZMWhVY0VOQlFtczU="
-
-echo "$PASSWORD" | docker login "$DOCKER_REGISTRY" -u "$USERNAME" --password-stdin
-
 app_name=$(xmllint --xpath "/[local-name()='project']/[local-name()='artifactId']/text()" pom.xml)
 
 version=$(xmllint --xpath "/[local-name()='project']/[local-name()='version']/text()" pom.xml)
