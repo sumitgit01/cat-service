44b0ac2790008c9b1ed634c485c729644f40e90c
sumitgit01 <sumitjoshi1988@gmail.com>
2025-12-06 17:24:30 +0530

    added build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+set -e
 
-app_name=$(xmllint --xpath "/[local-name()='project']/[local-name()='artifactId']/text()" pom.xml)
+app_name=$(xmllint --xpath "/*[local-name()='project']/*[local-name()='artifactId']/text()" pom.xml)
+version=$(xmllint --xpath "/*[local-name()='project']/*[local-name()='version']/text()" pom.xml)
 
-version=$(xmllint --xpath "/[local-name()='project']/[local-name()='version']/text()" pom.xml)
-docker build -t $app_name:$version .
-docker tag $app_name:$version trialcpnyac.jfrog.io/cat-docker/$app_name:$version
-docker push trialcpnyac.jfrog.io/cat-docker/$app_name:$version
+echo "App Name: $app_name"
+echo "Version: $version"
+
+docker build -t "$app_name:$version" .
+docker tag "$app_name:$version" "trialcpnyac.jfrog.io/cat-docker/$app_name:$version"
+docker push "trialcpnyac.jfrog.io/cat-docker/$app_name:$version"
