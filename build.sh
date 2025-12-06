47a670afb3760c8f53a699ed45f1e8630f856823
sumitgit01 <sumitjoshi1988@gmail.com>
2025-12-06 18:14:36 +0530

    Added build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 set -e
+ART_USER="amitajoshi1992@gmail.com"
+ART_API_KEY=$1
+ART_URL="https://trialcpnyac.jfrog.io/artifactory"
+REPO="CAT"
 mvn clean install -DskipTests=true
 
 app_name=$(xmllint --xpath "/*[local-name()='project']/*[local-name()='artifactId']/text()" pom.xml)
 version=$(xmllint --xpath "/*[local-name()='project']/*[local-name()='version']/text()" pom.xml)
-
+JAR_FILE="target/${app_name}-${version}.jar"
 echo "App Name: $app_name"
 echo "Version: $version"
-
+echo "JAR_FILE $JAR_FILE"
 docker build -t "$app_name:$version" .
 docker tag "$app_name:$version" "trialcpnyac.jfrog.io/cat-docker/$app_name:$version"
 docker push "trialcpnyac.jfrog.io/cat-docker/$app_name:$version"
+zip -r manifestbuild.zip manifestbuild/
+
+curl -u "${ART_USER}:${ART_API_KEY}" -T "${JAR_FILE}" "${ART_URL}/${REPO}/${app_name}/${version}/${app_name}-${version}.jar"
+
+curl -u "${ART_USER}:${ART_API_KEY}" -T manifestbuild.zip "${ART_URL}/${REPO}/${app_name}/${version}/manifestbuild.zip"
+
+echo "uploaded ${JAR_FILE} and manifestbuild.zip to ${ART_URL}/${REPO}/${app_name}/${version}/"
